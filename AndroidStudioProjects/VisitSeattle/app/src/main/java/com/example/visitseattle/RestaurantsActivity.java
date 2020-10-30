f4b18a81022f9861aa2ae0a1c84ad427ac82f305
Alina Stewart <alinastewart@Alinas-iMac.local>
2020-10-29 23:09:45 -0700

    Reformat all images. Add Seattle Space Needle to Museums category.

--- AndroidStudioProjects/VisitSeattle/app/src/main/java/com/example/visitseattle/RestaurantsActivity.java
+++ AndroidStudioProjects/VisitSeattle/app/src/main/java/com/example/visitseattle/RestaurantsActivity.java
@@ -16,12 +16,12 @@ public class RestaurantsActivity extends AppCompatActivity {
 
         // Create a list of places
         ArrayList<Place> seattlePlaces = new ArrayList<Place>();
-        seattlePlaces.add(new Place("Cafe Shampagne Restaurant", "(425) 666 222", R.drawable.restaurant_cafe_shampagne));
+        seattlePlaces.add(new Place("Cafe Champagne Restaurant", "(425) 666 222", R.drawable.restaurant_champagne));
         seattlePlaces.add(new Place("June Baby Restaurant","(425) 777 333", R.drawable.restaurant_june_baby));
         seattlePlaces.add(new Place("Restaurant Le Pichet", "(425) 888 333", R.drawable.restaurant_le_pichet));
         seattlePlaces.add(new Place("Restaurant Monsoon", "(425) 666 555", R.drawable.restaurant_monsoon));
         seattlePlaces.add(new Place("Restaurant Nue", "(425) 444 777", R.drawable.restaurant_nue));
-        seattlePlaces.add(new Place("The Pink Door Restaurant", "(425) 666 888", R.drawable.restaurant_the_pink_door));
+        seattlePlaces.add(new Place("The Pink Door Restaurant", "(425) 666 888", R.drawable.restaurant_pink_door));
 
 
         PlaceAdapter seattleAdapter = new PlaceAdapter(this, seattlePlaces);
